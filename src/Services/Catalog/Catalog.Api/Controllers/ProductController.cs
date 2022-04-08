51774f8717cbb67312a273f1d6df7ba2180b3ebf
mostafa kazemi <kazemi.mst@gmail.com>
2022-04-08 11:01:17 +0430

    improve Product controller and register service extensions

--- src/Services/Catalog/Catalog.Api/Controllers/ProductController.cs
+++ src/Services/Catalog/Catalog.Api/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Catalog.Api.Data;
 using Catalog.Api.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -10,32 +12,63 @@ using System.Threading.Tasks;
 namespace Catalog.Api.Controllers
 {
 
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
 
     public class ProductController : ControllerBase
     {
-        private readonly IClientSessionHandle _clientSessionHandle;
         private readonly IProductRepository _productRepository;
-
-        public ProductController(IClientSessionHandle clientSessionHandle,
-            IProductRepository productRepository)
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(
+            IProductRepository productRepository,
+            ILogger<ProductController> logger)
         {
-            _clientSessionHandle = clientSessionHandle;
             _productRepository = productRepository;
+            _logger = logger;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Product>))]
+        public async Task<IActionResult> Products()
         {
-           var products =  await _productRepository.GetAllAsync();
+            var products = await _productRepository.GetAllAsync();
             return Ok(products);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Creata([FromBody]Product product)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<IActionResult> Create([FromBody] Product product)
         {
-             await _productRepository.InsertAsync(product);
+            await _productRepository.InsertAsync(product);
+            return Ok();
+        }
+
+        [HttpPost("product/{id:length(24)}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById([FromRoute] string productId)
+        {
+            var product = await _productRepository.GetProductByIdAsync(productId);
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<IActionResult> Upldate([FromBody] Product product)
+        {
+            await _productRepository.UpdateAsync(product);
+            return Ok();
+        }
+
+
+        [HttpDelete("{productId:length(24)}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<IActionResult> Upldate([FromBody] string productId)
+        {
+            await _productRepository.DeleteAsync(productId);
             return Ok();
         }
     }
